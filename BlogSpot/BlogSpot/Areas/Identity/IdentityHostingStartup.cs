f364a1ee4af15a275097299870ce445da54c0557
MAURYA2609 <mauryapatel2609@gmail.com>
2020-11-11 17:35:40 +0530

    Maurya first init(simple login)

---
+++ BlogSpot/BlogSpot/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BlogSpot.Areas.Identity.Data;
+using BlogSpot.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlogSpot.Areas.Identity.IdentityHostingStartup))]
+namespace BlogSpot.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserContextConnection")));
+
+                services.AddDefaultIdentity<BlogSpotUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                })
+                    .AddEntityFrameworkStores<UserContext>();
+            });
+        }
+    }
+}
